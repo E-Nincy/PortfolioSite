e58cfbca9204954dd64504faee236d043f0a1226
E-Nincy <escotonincy10@outlook.es>
2025-09-12 09:46:57 +0200

    fix allowed hosts

--- PortfolioSite/settings.py
+++ PortfolioSite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',') if h.strip()]
 
 # Application definition
 
