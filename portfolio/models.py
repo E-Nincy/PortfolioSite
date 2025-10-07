83c3f8d6b1ae16d0bbe7a8bb187b77846f21d507
E-Nincy <escotonincy10@outlook.es>
2025-10-07 09:30:55 +0200

    change models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from cloudinary.models import CloudinaryField
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -18,14 +19,14 @@ class Project(models.Model):
 
     class Meta:
         ordering = ['position']
-    
+
 class Profile(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
     email = models.EmailField()
     github = models.URLField(blank=True, null=True)
     linkedin = models.URLField(blank=True, null=True)
-    photo = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return self.name
